a449f379a1f5f3921a91e6a6323fd9605e13ca2b
semteXKG <Klaus.Gradinger@gmail.com>
2025-01-03 18:41:28 +0100

    first commit

---
+++ proto_create.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+set -e
+
+GOOGLE_PROTOC="/usr/local/bin/protoc"
+NANO_PROTOC="/mnt/c/Users/local-semtex/esp32-idf-projects/nanopb-0.4.8-linux-x86/generator-bin/nanopb_generator"
+$NANO_PROTOC -D nano/ *.proto
+$GOOGLE_PROTOC --c_out=google/ message.proto
+
+cp nano/* ../funkbox-sender/src/proto/
+cp google/* ../funkbox-display/main/proto/
+cp google/* ../funkbox-base/main/proto/
+
+echo "Updated protobuf files"
+
+exit 0
